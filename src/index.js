b5c274535e21bfe29c7a2b4a43fbee1ba27d877c
Taras Dolynyuk <trsdln@gmail.com>
2018-04-29 14:44:36 +0300

    Fix host issue

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { HOST, PORT, UserSettings } from "./configuration";
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Node.JS: Hello World\n");
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>Node.JS: Hello World!</h1></body></html");
+  console.log(`${new Date()} Request handled: ${res.statusCode}`);
 });
 
-server.listen(PORT, HOST, () => {
+server.listen(PORT, () => {
   console.log(`Server running at http://${HOST}:${PORT}/`);
   console.log("Using user settings", UserSettings);
 });
